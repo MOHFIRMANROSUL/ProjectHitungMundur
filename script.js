10996c13367bd54558cedec4f5671b52dae0228a
MOH FIRMAN ROSUL <firmanrosulfirman@gmail.com>
2024-11-09 12:46:15 +0700

    stailing time calculations

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ let countdownDate = new Date("Nov 15, 2024 00:00:00").getTime();
 
 function startCountdown() {
     let countdownFunction = setInterval(function () {
-
-    }, 1000)
-};// Get the current date and time
         let now = new Date().getTime();
 
         // Find the distance between now and the countdown date
@@ -16,3 +13,8 @@ function startCountdown() {
         let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        
+    }, 1000)
+};// Get the current date and time
+        
